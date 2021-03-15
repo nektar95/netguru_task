5114b0f41cd04ce1c152d42705a6f4d14f713bb0
Aleksander Kaźmierczak <akazmierczak@surgecloud.co>
2021-03-15 20:28:51 +0100

    add themes and change splash image

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,16 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:netguru_task/home_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:netguru_task/utility/colors.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final _themeDataLight = ThemeData(
+      primaryColor: AppColors.main,
+      primaryColorLight: AppColors.mainLight,
+      backgroundColor: AppColors.backgroundLight,
+      accentColor: AppColors.textLight,
+      fontFamily: 'SFProText',
+    );
+
+    final _themeDataDark = ThemeData(
+      primaryColor: AppColors.main,
+      primaryColorLight: AppColors.mainLight,
+      backgroundColor: AppColors.backgroundDark,
+      accentColor: AppColors.textDark,
+      fontFamily: 'SFProText',
+    );
+
     return MaterialApp(
-      theme: ThemeData(
-        accentColor: const Color(0xFF1DC84F),
-      ),
+      theme: _themeDataLight,
+      darkTheme: _themeDataDark,
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         AppLocalizations.delegate,
