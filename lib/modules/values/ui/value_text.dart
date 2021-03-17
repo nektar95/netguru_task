e2078b4711bc3dddf1a7b07f1f1edf09922621ba
Aleksander Kaźmierczak <akazmierczak@surgecloud.co>
2021-03-17 23:18:24 +0100

    add main value text animation

--- lib/modules/values/ui/value_text.dart
+++ lib/modules/values/ui/value_text.dart
@@ -12,15 +12,27 @@ class ValueText extends StatelessWidget {
     return Shimmer.fromColors(
       baseColor: Theme.of(context).accentColor,
       highlightColor: Theme.of(context).shadowColor,
-      child: AutoSizeText(
-        text,
-        minFontSize: 20,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-            fontFamily: 'Caveat',
-            fontWeight: FontWeight.w700,
-            color: Theme.of(context).accentColor,
-            fontSize: 40),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 400),
+        transitionBuilder: (child, animation) {
+          return SlideTransition(
+            child: child,
+            position: Tween<Offset>(
+                    begin: const Offset(1.2, -2.4), end: const Offset(0.0, 0.0))
+                .animate(animation),
+          );
+        },
+        child: AutoSizeText(
+          text,
+          key: ValueKey<String>(text),
+          minFontSize: 20,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontFamily: 'Caveat',
+              fontWeight: FontWeight.w700,
+              color: Theme.of(context).accentColor,
+              fontSize: 40),
+        ),
       ),
     );
   }
